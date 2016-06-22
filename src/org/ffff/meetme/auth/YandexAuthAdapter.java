20f489a8753108c5dc5aed6bc6d9846ae6df3e56
flicus <flicus@gmail.com>
2016-06-22 12:56:14 +0300

    fix

--- src/org/ffff/meetme/auth/YandexAuthAdapter.java
+++ src/org/ffff/meetme/auth/YandexAuthAdapter.java
@@ -4,23 +4,6 @@ import org.ffff.meetme.model.Contact;
 
 import java.util.List;
 
-/**
- * Copyright (c) 2013 Amdocs jNetX.
- * http://www.amdocs.com
- * All rights reserved.
- * <p/>
- * This software is the confidential and proprietary information of
- * Amdocs jNetX. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license
- * agreement you entered into with Amdocs jNetX.
- * <p/>
- * User: Sergey Skoptsov (sskoptsov@amdocs.com)
- * Date: 23.01.14
- * Time: 18:25
- * <p/>
- * $Id:
- */
-
 public class YandexAuthAdapter implements AuthAdapter {
     @Override
     public String getAppId() {
